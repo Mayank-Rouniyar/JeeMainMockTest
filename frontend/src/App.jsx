f1d15c8dd57bf861211e00a4775183fffd957259
Mayank <mayankrouniyar9300@gmail.com>
2025-07-17 15:12:35 +0530

    Initial frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,30 +5,10 @@ import './App.css'
 import axios from 'axios'
 import { useEffect } from 'react'
 function App() {
-  const [jokes, setJokes] = useState([])
-  useEffect(()=>{
-    axios.get('/api/jokes')
-    .then((response)=>{
-     setJokes(response.data)
-    })
-    .catch((error)=>{
-      console.log("Error is",error)
-    })
-  })
   return (
     <>
-      <h1 className='text-3xl font-bold bg-gray-800 text-white p-4'>I am Mayank Rouniyar</h1>
-      <p>JOKES:{jokes.length}</p>
-      {
-        jokes.map((joke)=>(
-          <div key={joke.id}>
-            <h2>{joke.title}</h2>
-            <p>{joke.content}</p>
-          </div>
-        ))
-      }
+      <h1 className='bg-blue-700 text-3xl '>This is Mayank Rouniyar</h1>
     </>
   )
 }
-
 export default App
